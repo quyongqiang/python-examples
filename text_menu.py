f4f00d91de71d92134045033732bd65aa12aa1a6
Chaniel <Chaniel@users.noreply.github.com>
2014-09-12 10:06:01 +0800

    Create text_menu.py
    text menu program,

---
+++ text_menu.py
@@ -0,0 +1,48 @@
+#
+
+import sys
+
+menu = '''
+menu:
+    1: get summary of list.
+    2: get average of list.
+    x: exit the program.  '''
+menu_dic = {1:'add', 2:'avg'}
+def prompt():
+    print menu
+
+def add():
+    sum = 0
+    list1 = list(eval(raw_input("please input a list, at least two numbers:")))
+#     print 'ok'
+    for i in list1:
+        sum += i
+    print 'the sum of the list is:',sum
+
+def avg():
+    sum = 0
+    avg = 0
+    list1 = list(eval(raw_input("please input a list, at least two numbers:")))
+#     print 'ok'
+    for i in list1:
+        sum += i
+    avg = sum / float(len(list1))
+    print 'the average of the list is:',avg
+
+while True:
+    prompt()
+    cmd = raw_input('please input your choice:')
+    try:
+        cmd1 = int(cmd)
+        sum_list = eval(menu_dic[cmd1])()
+    except:
+        if cmd == 'x':
+            sys.exit()
+        else:
+            print 'input error'
+
+
+
+# print menu_dic[cmd1]
+
+
