44ec7167c325eb2493893adf32171674a384e121
Chaniel <Chaniel@users.noreply.github.com>
2014-10-04 21:29:35 +0800

    Create exchage_money.py

---
+++ numbers/exchage_money.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-  
+# 5-5
+"calculate how 105 cents can be made up"
+
+num = 105
+# res = list1[1,2]
+# print res[-1]
+
+
+# dollor have 1, 5, 10, 25 cents
+list1 = [1, 5, 10, 25]
+x = len(list1)
+print x
+
+
+def fun(num, x, l):
+#     print num / x
+    if x > 0:
+        x = x - 1
+    else:
+        return
+    res = num / l[x]
+    num = num % l[x]
+    print '%d * %d ' % (res, l[x])
+    fun(num, x, l)
+#    fun(num % x, list1.)
+
+a = fun(num, x, list1)
+
