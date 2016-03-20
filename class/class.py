589dc9dc4e89c780e4337a7cbe20dd240c41b5d9
chaniel <netqyq@gmail.com>
2016-03-20 08:13:26 +0800

    test local variablie, instance variable, class variable

---
+++ class/class.py
@@ -0,0 +1,35 @@
+class HelloWorld(object):
+	"""docstring for HelloWorld"""
+	# class variable
+	name = 1
+	def __init__(self, name):
+		super(HelloWorld, self).__init__()
+		self.name = name
+		
+
+	def greet(self):
+		print self.name
+
+	def test_name(self):
+		# local variable
+		name = "test_name function local var"
+		# instance variable
+		self.name = "lucy"
+		print name
+		print self.name
+		print HelloWorld.name
+
+
+	def pt_name(self):
+		name = "pt_name function local var"
+		print name
+		print self.name
+		print HelloWorld.name
+h1 = HelloWorld("lily")
+h1.greet()
+
+print ""
+h1.test_name()
+
+print ""
+h1.pt_name()
