bc55e4b5fcb112ea2f02cc94a948dd7a8712032c
Chaniel <netqyq@gmail.com>
2016-09-10 22:18:23 +0800

    mod get_http_header.py

--- urlopen/get_http_header.py
+++ urlopen/get_http_header.py
@@ -4,5 +4,12 @@ from urllib import urlopen
 doc = urlopen("http://www.baidu.com")    
 print doc.info()    
 
+
+print ("###")
 print doc.info().getheader('Content-Type')
 #print doc
+
+
+
+h_163 = urlopen("http://www.163.com")    
+print h_163.info() 
