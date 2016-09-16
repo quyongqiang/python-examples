8616676c4518749cd7563ba9c6cdb38688228517
Chaniel <netqyq@gmail.com>
2016-09-16 09:54:20 +0800

    chg closure.

--- closure/closure-counter.py
+++ closure/closure-counter.py
@@ -15,3 +15,24 @@ print count()
 print count()
 print count()
 
+
+	In [1]: def counter(start=0):
+	...:     count = [start]
+	...:     def incr():
+	...:         count[0] += 1
+	...:         return count[0]
+	...:     return incr
+	...:
+	In [2]: count = counter(5)
+
+	In [3]: count
+	Out[3]: <function __main__.incr>
+
+	In [4]: count
+	Out[4]: <function __main__.incr>
+
+	In [5]: print count()
+	6
+
+	In [6]: print count()
+	7
